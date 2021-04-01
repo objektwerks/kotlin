58832d902a3f1e7771ac68c971e2920f20b6bf25
objektwerks <objektwerks@runbox.com>
2021-04-01 08:42:56 -0400

    removed braces

--- src/main/kotlin/tripletail/App.kt
+++ src/main/kotlin/tripletail/App.kt
@@ -4,7 +4,7 @@ object App {
     fun greeting() = "Hello, Kotlin!"
 
     @JvmStatic fun main(args : Array<String>) {
-        args.forEach { arg -> println("Arg: ${arg}") }
+        args.forEach { arg -> println("Arg: $arg") }
         println("Greeting: ${greeting()}")
     }
 }
