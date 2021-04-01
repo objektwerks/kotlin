99e6541574f74a46097afa191c9e727e92b67a03
objektwerks <objektwerks@runbox.com>
2021-04-01 13:17:16 -0400

    refactored build.gradle.kts to reflect standard dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
