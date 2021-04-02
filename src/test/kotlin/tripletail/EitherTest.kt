2fcc19c9c9f19db84f64569fc8d403de934e164f
objektwerks <objektwerks@runbox.com>
2021-04-02 10:39:37 -0400

    added Either test

---
+++ src/test/kotlin/tripletail/EitherTest.kt
@@ -0,0 +1,23 @@
+package tripletail
+
+import arrow.core.*
+import arrow.core.Either.*
+
+import kotlin.test.Test
+
+class ToIntException(message: String) : Exception(message)
+
+class EitherTest {
+    private fun toInt(value: String): Either<ToIntException, Int> =
+        when ( val integer = value.toIntOrNull() ) {
+            null -> Left(ToIntException(value))
+            else    -> Right(integer)
+        }
+
+    @Test fun option() {
+        val list = listOf("1", "2", "3", "four")
+
+        val rights = list.map { toInt(it) }.filter { it.isRight() }
+        assert(rights == listOf(Right(1), Right(2), Right(3)))
+    }
+}
