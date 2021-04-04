d5f769c81ed28827c6fe8b9ddac5a58d5c1ecf5f
objektwerks <objektwerks@runbox.com>
2021-04-04 12:11:48 -0400

    refactored option test

--- src/test/kotlin/tripletail/OptionTest.kt
+++ src/test/kotlin/tripletail/OptionTest.kt
@@ -10,13 +10,12 @@ class OptionTest {
     @Test fun option() {
         val list = listOf("1", "2", "3", "four")
 
-        val options = list.map { toInt(it) }
-        assert( options == listOf(Some(1), Some(2), Some(3), None) )
+        assert( list.map { toInt(it) } == listOf(Some(1), Some(2), Some(3), None) )
 
-        val ints = list.map { toInt(it) }.filterOption()
-        assert( ints == listOf(1, 2, 3) )
+        assert( list.map { toInt(it) }.flatMap { it.toList() } == listOf(1, 2, 3) )
 
-        val sum = ints.sum()
-        assert( sum == 6 )
+        assert( list.map { toInt(it) }.filterOption() == listOf(1, 2, 3) )
+
+        assert( list.map { toInt(it) }.flatMap { it.toList() }.sum() == 6 )
     }
 }
