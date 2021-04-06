489bd8b54abbc1d88b358f0f9572d0f799e93166
objektwerks <objektwerks@runbox.com>
2021-04-06 15:49:51 -0400

    enhanced enum test

--- src/test/kotlin/tripletail/stdlib/EnumTest.kt
+++ src/test/kotlin/tripletail/stdlib/EnumTest.kt
@@ -2,18 +2,19 @@ package tripletail.stdlib
 
 import org.junit.Test
 
-enum class Beer {
-    pilsner,
-    lager,
+enum class Ale {
     paleAle,
     indianPaleAle,
-    stout,
-    wheat
+    doubleIndianPaleAle
 }
 
 class EnumTest {
     @Test fun enum() {
-        val beers = Beer.values()
+        assert( Ale.paleAle.toString() == "paleAle" )
+        assert( Ale.indianPaleAle.toString() == "indianPaleAle" )
+        assert( Ale.doubleIndianPaleAle.toString() == "doubleIndianPaleAle" )
+
+        val beers = Ale.values()
         beers.asList().forEach { beer -> assert( beers.contains( beer ) ) }
     }
 }
