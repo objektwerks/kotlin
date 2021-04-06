7b1fba249550341856acc5c364ccfb9df9e3bbac
objektwerks <objektwerks@runbox.com>
2021-04-06 11:21:56 -0400

    implemented basic list, set and map tests

--- src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
+++ src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
@@ -5,16 +5,19 @@ import org.junit.Test
 class CollectionsTest {
     @Test
     fun list() {
-
+        val list = listOf(1, 2, 3)
+        assert( list.size == 3 )
     }
 
     @Test
     fun set() {
-
+        val set = setOf(1, 2, 3)
+        assert( set.size == 3 )
     }
 
     @Test
     fun map() {
-
+        val map = mapOf( 1 to 1, 2 to 2, 3 to 3)
+        assert( map.size == 3 )
     }
 }
