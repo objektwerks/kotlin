793ab2013b13495c93b8fb1bd3d028b11aca2dba
objektwerks <objektwerks@runbox.com>
2021-04-06 06:32:02 -0400

    implemented basic ktor server app

--- src/main/kotlin/tripletail/KtorApp.kt
+++ src/main/kotlin/tripletail/KtorApp.kt
@@ -1,8 +1,21 @@
 package tripletail
 
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+import java.time.Instant
+
 object KtorApp {
     @JvmStatic fun main(args : Array<String>) {
-        args.forEach { arg -> println("Arg: $arg") }
-        println("Ktor app todo!")
+        embeddedServer(Netty, port = 7979) {
+            routing {
+                get("/") {
+                    call.respondText("Ktor datetime is: ${Instant.now()}")
+                }
+            }
+        }.start(wait = true)
     }
 }
