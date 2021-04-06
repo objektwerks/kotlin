ea415081aceeeaedd43bb4bc1bd55cb9c0368390
objektwerks <objektwerks@runbox.com>
2021-04-06 15:44:19 -0400

    enhanced collection test

--- src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
+++ src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
@@ -3,24 +3,21 @@ package tripletail.stdlib
 import org.junit.Test
 
 class CollectionsTest {
-    @Test
-    fun list() {
+    @Test fun list() {
         val list = listOf(1, 2, 3)
         assert( list.size == 3 )
         assert( list.map { it * it } == listOf(1, 4, 9) )
         assert( list.filter { it % 2 == 0 } == listOf(2) )
     }
 
-    @Test
-    fun set() {
+    @Test fun set() {
         val set = setOf(1, 2, 3)
         assert( set.size == 3 )
         assert( set.map { it * it } == listOf(1, 4, 9) )
         assert( set.filter { it % 2 == 0 } == listOf(2) )
     }
 
-    @Test
-    fun map() {
+    @Test fun map() {
         val map = mapOf(1 to 1, 2 to 2, 3 to 3)
         assert( map.size == 3 )
         assert( map.map { it.value * it.value } == listOf(1, 4, 9) )
