ca67b567f8455c42351212a7d10c0794b55673b9
objektwerks <objektwerks@runbox.com>
2021-04-06 15:28:52 -0400

    enhanced collection test, added map

--- src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
+++ src/test/kotlin/tripletail/stdlib/CollectionsTest.kt
@@ -7,17 +7,20 @@ class CollectionsTest {
     fun list() {
         val list = listOf(1, 2, 3)
         assert( list.size == 3 )
+        assert( list.map { it * it } == listOf(1, 4, 9) )
     }
 
     @Test
     fun set() {
         val set = setOf(1, 2, 3)
         assert( set.size == 3 )
+        assert( set.map { it * it } == listOf(1, 4, 9) )
     }
 
     @Test
     fun map() {
         val map = mapOf( 1 to 1, 2 to 2, 3 to 3)
         assert( map.size == 3 )
+        assert( map.map { it.value * it.value } == listOf(1, 4, 9) )
     }
 }
