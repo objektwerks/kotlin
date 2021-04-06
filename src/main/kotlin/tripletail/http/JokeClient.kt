2b513550dc331053ad3f1f37ed739d95e5c510eb
objektwerks <objektwerks@runbox.com>
2021-04-06 07:54:01 -0400

    implemented ktor client

---
+++ src/main/kotlin/tripletail/http/JokeClient.kt
@@ -0,0 +1,14 @@
+package tripletail.http
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+suspend fun main() {
+    val client = HttpClient(CIO)
+    val response: HttpResponse = client.get("http://api.icndb.com/jokes/random/")
+    println(response.status)
+    println(response.readText())
+    client.close()
+}
