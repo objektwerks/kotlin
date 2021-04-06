e067200cd7e6ec54b5bf05c22fbc2127f9310988
objektwerks <objektwerks@runbox.com>
2021-04-06 07:34:00 -0400

    renamed ktor app to NowServer; refactored routing

--- src/main/kotlin/tripletail/http/NowServer.kt
+++ src/main/kotlin/tripletail/http/NowServer.kt
@@ -8,11 +8,11 @@ import io.ktor.server.netty.*
 
 import java.time.Instant
 
-/** To test server: curl http://localhost:7979/ */
+/** To test server: curl http://localhost:7979/now */
 fun main() {
     embeddedServer(Netty, port = 7979) {
         routing {
-            get("/") {
+            get("/now") {
                 call.respondText("Ktor datetime is: ${Instant.now()}")
             }
         }
