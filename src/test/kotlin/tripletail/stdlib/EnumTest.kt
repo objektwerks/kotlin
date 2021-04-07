41f3351027b918717f5e5353797377843f4b4f6c
objektwerks <objektwerks@runbox.com>
2021-04-07 07:45:10 -0400

    enhanced enum test

--- src/test/kotlin/tripletail/stdlib/EnumTest.kt
+++ src/test/kotlin/tripletail/stdlib/EnumTest.kt
@@ -14,6 +14,10 @@ class EnumTest {
         assert( Ale.indianPaleAle.toString() == "indianPaleAle" )
         assert( Ale.doubleIndianPaleAle.toString() == "doubleIndianPaleAle" )
 
+        assert( Ale.valueOf("paleAle") == Ale.paleAle )
+        assert( Ale.valueOf("indianPaleAle") == Ale.indianPaleAle )
+        assert( Ale.valueOf("doubleIndianPaleAle") == Ale.doubleIndianPaleAle )
+
         val beers = Ale.values()
         assert( beers.size == 3 )
         beers.asList().forEach { beer -> assert( beers.contains( beer ) ) }
