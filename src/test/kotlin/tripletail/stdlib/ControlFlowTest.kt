9612bcb30b2afc447e90bb7ddb886937598b5720
objektwerks <objektwerks@runbox.com>
2021-04-08 07:36:08 -0400

    enhanced control flow test

--- src/test/kotlin/tripletail/stdlib/ControlFlowTest.kt
+++ src/test/kotlin/tripletail/stdlib/ControlFlowTest.kt
@@ -7,5 +7,12 @@ import org.junit.Test
 class ControlFlowTest {
     @Test fun controlFlow() {
         assert( ( if ( Random(10).nextInt() > 5 ) 6 else 4 ) > 3 )
+
+        assert(
+            when (val n = Random(10).nextInt()) {
+                0 -> 5
+                else -> n
+            } != 0
+        )
     }
 }
