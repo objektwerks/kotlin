6a853bc7a1f2686544c17afdf03fdc75067bf538
objektwerks <objektwerks@runbox.com>
2021-04-08 09:12:39 -0400

    enhanced interface test

--- src/test/kotlin/tripletail/stdlib/InterfaceTest.kt
+++ src/test/kotlin/tripletail/stdlib/InterfaceTest.kt
@@ -2,13 +2,17 @@ package tripletail.stdlib
 
 import org.junit.Test
 
+enum class ShapeType { circle }
+
 interface Shape {
+    val typeOf: ShapeType
     fun draw(): String
 }
 
 class InterfaceTest {
     @Test fun interfaces() {
         class Circle : Shape {
+            override val typeOf = ShapeType.circle
             override fun draw(): String = "circle"
         }
         assert( Circle().draw() == "circle" )
