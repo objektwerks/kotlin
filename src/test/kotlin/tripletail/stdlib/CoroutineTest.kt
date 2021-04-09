23721c06495885f5f586c080972aafedc6809ed3
objektwerks <objektwerks@runbox.com>
2021-04-09 12:25:59 -0400

    enhanced coroutine test

--- src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
+++ src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
@@ -6,7 +6,10 @@ import org.junit.Test
 
 class CoroutineTest {
     @Test fun coroutine() {
-        fun sum(x: Int, y: Int): Int = x + y
-        assert( runBlocking { sum(3, 3) } == 6 )
+        fun sum(x: Int, y: Int): Int = runBlocking { x + y }
+        assert( sum(3, 3) == 6 )
+
+        suspend fun multiply(x: Int, y: Int): Int = withContext(Dispatchers.Default) { x * y }
+        assert( runBlocking { multiply(3, 3) } == 9 )
     }
 }
