f59b0d7151d3666dc6099df7ec56887afa3019a5
objektwerks <objektwerks@runbox.com>
2021-04-09 12:32:00 -0400

    enhanced coroutine test

--- src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
+++ src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
@@ -11,5 +11,11 @@ class CoroutineTest {
 
         suspend fun multiply(x: Int, y: Int): Int = withContext(Dispatchers.Default) { x * y }
         assert( runBlocking { multiply(3, 3) } == 9 )
+
+        val deferred = (1..10).map { n ->
+            GlobalScope.async { n }
+        }
+        suspend fun sumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
+        assert( runBlocking { sumOf(deferred) } == 10 )
     }
 }
