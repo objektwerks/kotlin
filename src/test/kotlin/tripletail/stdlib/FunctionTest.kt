43dfc98260bce9e5aa4065d6f0ae3666faad2d79
objektwerks <objektwerks@runbox.com>
2021-04-09 08:57:08 -0400

    enhanced function test

--- src/test/kotlin/tripletail/stdlib/FunctionTest.kt
+++ src/test/kotlin/tripletail/stdlib/FunctionTest.kt
@@ -3,8 +3,7 @@ package tripletail.stdlib
 import org.junit.Test
 
 class FunctionTest {
-    @Test
-    fun function() {
+    @Test fun function() {
         fun square(n: Int): Int = n * n
         assert( square(2) == 4 )
 
@@ -14,5 +13,11 @@ class FunctionTest {
             return result
         }
         assert( toList(1, 2, 3) == listOf(1, 2, 3) )
+
+        class Numbers {
+            infix fun cube(n: Int): Int = n * n * n
+        }
+        val numbers = Numbers()
+        assert( numbers cube 3 == 27 )
     }
 }
