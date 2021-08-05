a90d55c3ed04ea6bd4643d4ad6722274f8ad1e84
objektwerks <objektwerks@runbox.com>
2021-08-05 07:06:12 -0400

    corrected coroutine test

--- src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
+++ src/test/kotlin/tripletail/stdlib/CoroutineTest.kt
@@ -5,17 +5,14 @@ import kotlinx.coroutines.*
 import org.junit.Test
 
 class CoroutineTest {
+    fun sum(x: Int, y: Int): Int = runBlocking { x + y }
+    suspend fun multiply(x: Int, y: Int): Int = withContext(Dispatchers.Default) { x * y }
+    suspend fun deferred(): List<Deferred<Int>> = (1..10).map { n -> coroutineScope { async { n + 1 } } }
+    suspend fun sumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
+
     @Test fun coroutine() {
-        fun sum(x: Int, y: Int): Int = runBlocking { x + y }
         assert( sum(3, 3) == 6 )
-
-        suspend fun multiply(x: Int, y: Int): Int = withContext(Dispatchers.Default) { x * y }
         assert( runBlocking { multiply(3, 3) } == 9 )
-
-        val deferred = (1..10).map { n ->
-            GlobalScope.async { n }
-        }
-        suspend fun sumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
-        assert( runBlocking { sumOf(deferred) } == 55 )
+        assert( runBlocking { sumOf( deferred() ) } == 65 )
     }
 }
