af1d6b0a18821a2c6306880c145c235ecaeed28e
objektwerks <objektwerks@runbox.com>
2022-04-11 18:10:07 -0400

    refactored ktor 2.0 now server

--- src/main/kotlin/tripletail/http/NowServer.kt
+++ src/main/kotlin/tripletail/http/NowServer.kt
@@ -1,10 +1,10 @@
 package tripletail.http
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 import java.time.Instant
 
