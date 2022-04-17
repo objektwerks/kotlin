13143576194d1e3b92cb38bd766877d31485d278
objektwerks <objektwerks@runbox.com>
2022-04-17 09:56:05 -0400

    implemented context receiver test

--- src/test/kotlin/stdlib/ContextReceiverTest.kt
+++ src/test/kotlin/stdlib/ContextReceiverTest.kt
@@ -3,7 +3,25 @@ package stdlib
 import org.junit.Test
 
 class ContextReceiverTest {
-    @Test fun context() {
+    class Logger(private val name: String) {
+        fun log(message: String): Unit = println("$name: $message")
+    }
+    interface LoggerContext {
+        val log: Logger
+    }
 
+    context(LoggerContext)
+    private fun store(value: String): Boolean {
+        val logger = Logger("test")
+        logger.log("stored: $value")
+        return true
+    }
+
+    @Test fun context() {
+        fun test(loggingContext: LoggerContext) {
+            with(loggingContext) {
+                assert(store("test"))
+            }
+        }
     }
 }
