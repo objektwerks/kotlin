782577576fb175af4a7d8042a52a2c85e0ad21d6
objektwerks <objektwerks@runbox.com>
2022-04-18 14:34:50 -0400

    reformatted collection test code

--- src/test/kotlin/stdlib/CollectionTest.kt
+++ src/test/kotlin/stdlib/CollectionTest.kt
@@ -48,8 +48,10 @@ class CollectionTest {
         val set = setOf(1, 2, 3)
         assert( set.size == 3 )
         assert( set.contains(1) )
+
         assert( setOf(1, 2, 3, 4) == set + 4 )
         assert( setOf(1, 2) == set - 3 )
+
         assert( set.map { it * it } == listOf(1, 4, 9) )
         assert( set.filter { it % 2 == 0 } == listOf(2) )
         assert( set.zip( listOf(4, 5, 6) ) == listOf( Pair(1, 4), Pair(2, 5), Pair(3, 6)) )
@@ -67,8 +69,10 @@ class CollectionTest {
         val map = mapOf(1 to 1, 2 to 2, 3 to 3)
         assert( map[1] == 1 )
         assert( map.size == 3 )
+
         assert( mapOf(1 to 1, 2 to 2) == mapOf(1 to 1 ) + (2 to 2) )
         assert( mapOf(1 to 1) == mapOf(1 to 1, 2 to 2) - 2 )
+
         assert( map.map { it.value * it.value } == listOf(1, 4, 9) )
         assert( map.filter { it.value % 2 == 0 } == mapOf(2 to 2) )
         assert( map.zip( mapOf(1 to 4, 2 to 5, 3 to 6) ) == mapOf(1 to Pair(1, 4), 2 to Pair(2, 5), 3 to Pair(3, 6)) )
