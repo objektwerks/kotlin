db5b238b8d3bc0765a5a23beac840145c268c31d
objektwerks <objektwerks@runbox.com>
2022-04-18 08:18:32 -0400

    edited annotation location

--- src/test/kotlin/stdlib/FunctionTest.kt
+++ src/test/kotlin/stdlib/FunctionTest.kt
@@ -3,8 +3,7 @@ package stdlib
 import org.junit.Test
 
 class FunctionTest {
-    @Test
-    fun function() {
+    @Test fun function() {
         fun square(n: Int): Int = n * n
         assert(square(2) == 4)
 
