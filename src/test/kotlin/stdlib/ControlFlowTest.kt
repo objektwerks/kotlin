9a58aa948cbf58a853373ca860bcf150776197b8
objektwerks <objektwerks@runbox.com>
2022-04-18 09:44:00 -0400

    extracted match/when test in control flow test

--- src/test/kotlin/stdlib/ControlFlowTest.kt
+++ src/test/kotlin/stdlib/ControlFlowTest.kt
@@ -8,6 +8,23 @@ class ControlFlowTest {
     @Test fun controlFlow() {
         assert( (if (Random(10).nextInt() > 5) 6 else 4) > 3 )
 
+        for (n in listOf(1, 2, 3)) { assert(n > 0) }
+        for (n in 1..3) { assert(n > 0) }
+
+        var condition = 3
+        while (condition > 0) {
+            condition--
+        }
+        assert( condition == 0 )
+
+        condition = 3
+        do {
+            condition--
+        } while (condition > 0)
+        assert( condition == 0 )
+    }
+
+    @Test fun match() {
         assert(
             when (val n = Random(10).nextInt()) {
                 0 -> 5
@@ -26,24 +43,8 @@ class ControlFlowTest {
         assert(
             when (val any: Any = Random(10).nextInt().toString()) {
                 is String -> true
-                is Int -> false
                 else -> any.hashCode() == 0
             }
         )
-
-        for (n in listOf(1, 2, 3)) { assert(n > 0) }
-        for (n in 1..3) { assert(n > 0) }
-
-        var condition = 3
-        while (condition > 0) {
-            condition--
-        }
-        assert( condition == 0 )
-
-        condition = 3
-        do {
-            condition--
-        } while (condition > 0)
-        assert( condition == 0 )
     }
 }
