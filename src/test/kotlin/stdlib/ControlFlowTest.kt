c49246c7b1c2cd6ac824df8f13687b5c05254ecd
objektwerks <objektwerks@runbox.com>
2022-04-18 09:45:49 -0400

    extracted if else test in control flow test

--- src/test/kotlin/stdlib/ControlFlowTest.kt
+++ src/test/kotlin/stdlib/ControlFlowTest.kt
@@ -6,8 +6,6 @@ import org.junit.Test
 
 class ControlFlowTest {
     @Test fun controlFlow() {
-        assert( (if (Random(10).nextInt() > 5) 6 else 4) > 3 )
-
         for (n in listOf(1, 2, 3)) { assert(n > 0) }
         for (n in 1..3) { assert(n > 0) }
 
@@ -22,6 +20,11 @@ class ControlFlowTest {
             condition--
         } while (condition > 0)
         assert( condition == 0 )
+    }
+
+    @Test fun ifelse() {
+        val result = if (Random(10).nextInt() > 5) 6 else 4
+        assert( result > 3 )
     }
 
     @Test fun match() {
