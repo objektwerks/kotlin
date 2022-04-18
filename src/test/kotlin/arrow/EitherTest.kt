db6f060c82683f9e1acd39f834e46d841cff78af
objektwerks <objektwerks@runbox.com>
2022-04-18 09:58:01 -0400

    made fun private

--- src/test/kotlin/arrow/EitherTest.kt
+++ src/test/kotlin/arrow/EitherTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.Test
 class ToIntException(message: String) : Exception(message)
 
 class EitherTest {
-    fun toInt(value: String): Either<ToIntException, Int> =
+    private fun toInt(value: String): Either<ToIntException, Int> =
         when ( val integer = value.toIntOrNull() ) {
             null -> Left(ToIntException(value))
             else -> Right(integer)
