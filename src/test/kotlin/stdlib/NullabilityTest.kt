115de8105b9824759241784141ec77cddfe74748
objektwerks <objektwerks@runbox.com>
2022-04-18 15:55:04 -0400

    edited nullability test

--- src/test/kotlin/stdlib/NullabilityTest.kt
+++ src/test/kotlin/stdlib/NullabilityTest.kt
@@ -1,34 +1,30 @@
 package stdlib
 
-import kotlin.test.assertNull
 import kotlin.test.assertFailsWith
-import kotlin.test.assertNotNull
 
 import org.junit.Test
 
-data class Task(val description: String, val code: String? = null)
-
 class NullabilityTest {
-    @Test fun nullability() {
+    @Test fun nullable() {
         val text: String? = null
-        assert( text.isNullOrEmpty() )
-        assert( (text?.length ?: -1) == -1)
+        assert(text.isNullOrEmpty())
+        assert((text?.length ?: -1) == -1)
         assertFailsWith<NullPointerException> {
             text!!.length
         }
+    }
 
+    @Test fun notNullable() {
         val value = "value"
-        assert( value.isNotEmpty() )
+        assert(value.isNotEmpty())
+    }
 
-        val task = Task("Mow yard.")
-        assertNull( task.code )
-        assertNotNull( task.copy(code = "completed").code )
-
+    @Test fun letNullable() {
         val numbers: List<Int?> = listOf(1, 2, 3, null)
         var squares = listOf<Int?>()
-        for (item in numbers) {
-            item?.let { squares = squares.plus(it * it); it }
-            ?.also { println("number: $it") }
+        for (number in numbers) {
+            number?.let { squares = squares.plus(it * it); it }
+            ?.also { println(it) }
         }
         assert( squares == listOf( 1, 4, 9) )
         assert( numbers.filterNotNull() == listOf( 1, 2, 3) )
