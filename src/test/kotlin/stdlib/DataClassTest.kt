5c2795be8dfdf5ab5f8bc34ff520436b935703f1
objektwerks <objektwerks@runbox.com>
2022-04-18 09:31:07 -0400

    edited data class test

--- src/test/kotlin/stdlib/DataClassTest.kt
+++ src/test/kotlin/stdlib/DataClassTest.kt
@@ -8,7 +8,7 @@ class DataClassTest {
     @Test fun dataClass() {
         data class Data(val value: String)
         val data = Data("data")
-        val copy = data.copy( value = "copy")
+        val copy = data.copy(value = "copy")
         val ( destructuredValue ) = copy
         assertEquals( copy.value, destructuredValue )
         assertNotEquals( data, copy )
