adbb2d3f899221eb44972075a7dd353d6e690004
objektwerks <objektwerks@runbox.com>
2022-04-18 09:36:41 -0400

    edited coroutine test

--- src/test/kotlin/stdlib/CoroutineTest.kt
+++ src/test/kotlin/stdlib/CoroutineTest.kt
@@ -5,10 +5,18 @@ import kotlinx.coroutines.*
 import org.junit.Test
 
 class CoroutineTest {
-    fun sum(x: Int, y: Int): Int = runBlocking { x + y }
-    suspend fun multiply(x: Int, y: Int): Int = withContext(Dispatchers.Default) { x * y }
-    suspend fun deferred(): List<Deferred<Int>> = (1..10).map { n -> coroutineScope { async { n + 1 } } }
-    suspend fun sumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
+    private fun sum(x: Int, y: Int): Int = runBlocking { x + y }
+
+    private suspend fun multiply(x: Int, y: Int): Int = withContext( Dispatchers.Default ) { x * y }
+
+    private suspend fun sumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
+
+    private suspend fun deferred(): List<Deferred<Int>> =
+        (1..10).map { n ->
+            coroutineScope {
+                async { n + 1 }
+            }
+        }
 
     @Test fun coroutine() {
         assert( sum(3, 3) == 6 )
