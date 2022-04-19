aaf76ba51363f4611a7b3df5e9e51354ee12e49f
objektwerks <objektwerks@runbox.com>
2022-04-19 15:41:01 -0400

    moved interface test to class test

--- src/test/kotlin/stdlib/ClassTest.kt
+++ src/test/kotlin/stdlib/ClassTest.kt
@@ -128,6 +128,26 @@ class ClassTest {
         assert( eval( Sum( Const(1), Const(2) ) ) == 3)
     }
 
+    enum class ShapeType { circle }
+    interface Shape {
+        val typeOf: ShapeType
+        fun draw(): String
+    }
+    fun interface IntPredicate {
+        fun accept(i: Int): Boolean
+    }
+
+    @Test fun interfaces() {
+        class Circle : Shape {
+            override val typeOf = ShapeType.circle
+            override fun draw(): String = "circle"
+        }
+        assert( Circle().draw() == "circle" )
+
+        val isEven = IntPredicate { it % 2 == 0 }
+        assertTrue( isEven.accept(2) )
+    }
+
     interface Car {
         fun race(): String
     }
