0c0db2674406772804659a36eab232b96292b8f0
objektwerks <objektwerks@runbox.com>
2022-04-19 09:36:22 -0400

    rearranged code in sealed class test

--- src/test/kotlin/stdlib/SealedClassTest.kt
+++ src/test/kotlin/stdlib/SealedClassTest.kt
@@ -2,19 +2,19 @@ package stdlib
 
 import org.junit.Test
 
-sealed class Expr
-class Const(val value: Int) : Expr()
-class Sum(val left: Expr, val right: Expr) : Expr()
-object NotANumber : Expr()
-
-fun eval(expr: Expr): Int =
-    when (expr) {
-        is Const -> expr.value
-        is Sum -> eval(expr.right) + eval(expr.left)
-        NotANumber -> Double.NaN.toInt()
-    }
-
 class SealedClassTest {
+    sealed class Expr
+    class Const(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr()
+    object NotANumber : Expr()
+
+    private fun eval(expr: Expr): Int =
+        when (expr) {
+            is Const -> expr.value
+            is Sum -> eval(expr.right) + eval(expr.left)
+            NotANumber -> Double.NaN.toInt()
+        }
+
     @Test fun sealedClass() {
         assert( eval( Sum( Const(1), Const(2) ) ) == 3)
     }
