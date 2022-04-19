6135be5249f0f952e6f25671835da7a7f689286c
objektwerks <objektwerks@runbox.com>
2022-04-19 15:55:53 -0400

    readded interface test

---
+++ src/test/kotlin/stdlib/InterfaceTest.kt
@@ -0,0 +1,32 @@
+package stdlib
+
+import kotlin.test.assertTrue
+
+import org.junit.Test
+
+class InterfaceTest {
+    enum class ShapeType { circle }
+    interface Shape {
+        val typeOf: ShapeType
+        fun draw(): String
+    }
+
+    @Test
+    fun byInterface() {
+        class Circle : Shape {
+            override val typeOf = ShapeType.circle
+            override fun draw(): String = "circle"
+        }
+        assert(Circle().draw() == "circle")
+    }
+
+    fun interface IntPredicate {
+        fun accept(i: Int): Boolean
+    }
+
+    @Test
+    fun functionInterface() {
+        val isEven = IntPredicate { it % 2 == 0 }
+        assertTrue( isEven.accept(2) )
+    }
+}
