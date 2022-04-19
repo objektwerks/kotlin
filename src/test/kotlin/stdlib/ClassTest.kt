d83572ad76b99494f4a4199bd4f75872a07ae287
objektwerks <objektwerks@runbox.com>
2022-04-19 09:37:23 -0400

    moved sealed class test to class test

--- src/test/kotlin/stdlib/ClassTest.kt
+++ src/test/kotlin/stdlib/ClassTest.kt
@@ -78,4 +78,20 @@ class ClassTest {
         }
         assert( greeting.message() == "Hello, Kotlin!")
     }
+
+    sealed class Expr
+    class Const(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr()
+    object NotANumber : Expr()
+
+    private fun eval(expr: Expr): Int =
+        when (expr) {
+            is Const -> expr.value
+            is Sum -> eval(expr.right) + eval(expr.left)
+            NotANumber -> Double.NaN.toInt()
+        }
+
+    @Test fun sealedClass() {
+        assert( eval( Sum( Const(1), Const(2) ) ) == 3)
+    }
 }
