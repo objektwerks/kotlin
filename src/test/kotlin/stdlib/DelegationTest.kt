0d72859bbc73395dfac1bcb150b25fb4b3be8aff
objektwerks <objektwerks@runbox.com>
2022-04-19 10:41:08 -0400

    refactored delegation test

--- src/test/kotlin/stdlib/DelegationTest.kt
+++ src/test/kotlin/stdlib/DelegationTest.kt
@@ -2,16 +2,18 @@ package stdlib
 
 import org.junit.Test
 
-interface SportsCar {
-    fun race(): String
-}
-
 class DelegationTest {
+    interface SportsCar {
+        fun race(): String
+    }
+
+    class Porsche : SportsCar {
+        override fun race(): String = "prrrr"
+    }
+
+    class DerivedPorsche(derived: SportsCar) : SportsCar by derived
+
     @Test fun delegation() {
-        class Porsche : SportsCar {
-            override fun race(): String = "prrrr"
-        }
-        class DerivedPorsche(derived: SportsCar) : SportsCar by derived
         assert( DerivedPorsche( Porsche() ).race() == "prrrr" )
     }
 }
