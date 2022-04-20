e3f25fc7e27f466999982cdcca8146b4f0b205e9
objektwerks <objektwerks@runbox.com>
2022-04-20 07:44:45 -0400

    refactored coroutine test

--- src/test/kotlin/stdlib/CoroutineTest.kt
+++ src/test/kotlin/stdlib/CoroutineTest.kt
@@ -6,22 +6,24 @@ import org.junit.Test
 
 @Suppress("SameParameterValue")
 class CoroutineTest {
-    private fun runBlockingSum(x: Int, y: Int): Int = runBlocking { x + y }
-
-    private suspend fun withContextSum(x: Int, y: Int): Int = withContext( Dispatchers.Default ) { x + y }
-
-    private suspend fun deferredSumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
-
-    private suspend fun deferredAsyncSource(): List<Deferred<Int>> =
-        (1..10).map { n ->
-            coroutineScope {
-                async { n + 1 }
-            }
-        }
-
-    @Test fun coroutine() {
+    @Test fun runBlockingSum() {
+        fun runBlockingSum(x: Int, y: Int): Int = runBlocking { x + y }
         assert( runBlockingSum(1, 2) == 3 )
+    }
+
+    @Test fun withContextSum() {
+        suspend fun withContextSum(x: Int, y: Int): Int = withContext( Dispatchers.Default ) { x + y }
         assert( runBlocking { withContextSum(1, 2) } == 3 )
+    }
+
+    @Test fun deferredSumOf() {
+        suspend fun deferredSumOf(deferred: List<Deferred<Int>>): Int = deferred.sumOf { it.await() }
+        suspend fun deferredAsyncSource(): List<Deferred<Int>> =
+            (1..10).map { n ->
+                coroutineScope {
+                    async { n + 1 }
+                }
+            }
         assert( runBlocking { deferredSumOf( deferredAsyncSource() ) } == 65 )
     }
 }
