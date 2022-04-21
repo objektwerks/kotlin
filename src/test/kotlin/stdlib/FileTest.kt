45ef6bc934c6a3513118d8cdbdab5e3966ac186a
objektwerks <objektwerks@runbox.com>
2022-04-21 15:27:00 -0400

    added resource as stream test

--- src/test/kotlin/stdlib/FileTest.kt
+++ src/test/kotlin/stdlib/FileTest.kt
@@ -29,4 +29,8 @@ class FileTest {
     @Test fun resource() {
         assert( this::class.java.getResource("/config.yaml").readText().isNotEmpty() )
     }
+
+    @Test fun resourceAsStream() {
+        assert( this::class.java.getResourceAsStream("/config.yaml").bufferedReader().readLines().count() == 2 )
+    }
 }
