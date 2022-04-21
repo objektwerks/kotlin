1013aaf94114bee0e973eb3bfb689c2d83ba621e
objektwerks <objektwerks@runbox.com>
2022-04-21 13:03:07 -0400

    split generic test

--- src/test/kotlin/stdlib/TypeTest.kt
+++ src/test/kotlin/stdlib/TypeTest.kt
@@ -3,26 +3,28 @@ package stdlib
 import org.junit.Test
 
 class TypeTest {
-    @Test fun generic() {
+    @Test fun genericClass() {
         class Box<T>(t: T) {
             val value = t
         }
         assert( Box(3).value == 3 )
+    }
 
+    @Test fun genericFunction() {
         fun <T> singletonList(item: T): List<T> = listOf(item)
         assert( singletonList(3) == listOf(3) )
     }
 
     @Test fun outin() {
         class Producer<out T>(private val t: T) {
-            fun get(): T = t
+            fun yield(): T = t
         }
         class Consumer<in T> {
-            fun toString(value: T): String = value.toString()
+            fun asString(value: T): String = value.toString()
         }
 
-        val source = Producer(3).get()
-        val sink = Consumer<Int>().toString(source)
+        val source = Producer(3).yield()
+        val sink = Consumer<Int>().asString(source)
         assert(source == 3 && sink == "3")
     }
 }
