8043a8f6f247fe7c25d55f7e0c61514d4ddd54ef
objektwerks <objektwerks@runbox.com>
2022-04-21 08:06:11 -0400

    edited console app

--- src/main/kotlin/console/App.kt
+++ src/main/kotlin/console/App.kt
@@ -1,10 +1,13 @@
 package console
 
+import java.time.Instant
+
 object App {
-    fun greeting() = "Hello, Kotlin!"
+    private fun greeting(): String = "Kotlin!"
 
     @JvmStatic fun main(args : Array<String>) {
         args.forEach { arg -> println("Arg: $arg") }
-        println("Greeting: ${greeting()}")
+        println("Greetings, ${greeting()}")
+        println("Datetime: ${Instant.now().toString()}")
     }
 }
