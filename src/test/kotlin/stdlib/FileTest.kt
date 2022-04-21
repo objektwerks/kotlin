e245d2625d32751f351b8fd603691e2c80e760c7
objektwerks <objektwerks@runbox.com>
2022-04-21 15:07:53 -0400

    added write read bytes test to file test

--- src/test/kotlin/stdlib/FileTest.kt
+++ src/test/kotlin/stdlib/FileTest.kt
@@ -5,9 +5,16 @@ import java.io.File
 
 class FileTest {
     @Test fun writeReadText() {
-        val path = "./build/test.txt"
+        val path = "./build/text.txt"
         val text = "test write"
         File(path).writeText(text)
         assert( File(path).readText() == text )
+    }
+
+    @Test fun writeReadBytes() {
+        val path = "./build/bytes.txt"
+        val bytes = "test write".encodeToByteArray()
+        File(path).writeBytes(bytes)
+        assert( File(path).readBytes().decodeToString() == bytes.decodeToString() )
     }
 }
