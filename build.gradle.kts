23e094a1fb43971d6ed02e7bb2c8dbdfcb707d98
objektwerks <objektwerks@runbox.com>
2022-04-27 15:12:24 -0400

    upgraded exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,9 +41,9 @@ dependencies {
     implementation("io.ktor:ktor-server-core:2.0.0")
     implementation("io.ktor:ktor-server-netty:2.0.0")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
+    implementation("org.jetbrains.exposed:exposed-core:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
 
     implementation("io.arrow-kt:arrow-core:1.1.2")
 
