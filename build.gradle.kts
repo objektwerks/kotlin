969fac26eae6b7f86ea3fb09b625e5343755229c
objektwerks <objektwerks@runbox.com>
2022-12-08 11:27:43 -0500

    upgraded kotlin stdlib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.22"
     application
     id("com.adarshr.test-logger") version "3.2.0"
 }
@@ -27,10 +27,10 @@ repositories {
 
 dependencies {
     implementation(platform(kotlin("bom")))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.22")
 
-    implementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-test:1.7.22")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.22")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
