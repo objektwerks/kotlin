25a1f584ddf862aba06853d8fa8994ae5e205040
objektwerks <objektwerks@runbox.com>
2023-01-31 08:34:29 -0500

    edited build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "19"
     kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
 
