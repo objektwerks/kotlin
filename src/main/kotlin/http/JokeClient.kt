bcdacb0b249f49a385d3c97ce58e622066ba3757
objektwerks <objektwerks@runbox.com>
2023-01-31 14:04:58 -0500

    enhanced joke client

--- src/main/kotlin/http/JokeClient.kt
+++ src/main/kotlin/http/JokeClient.kt
@@ -5,10 +5,16 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+
 suspend fun main() {
     val client = HttpClient(CIO)
     val response = client.get("https://api.chucknorris.io/jokes/random")
-    println(response.status)
-    println(response.bodyAsText())
+    val json = response.bodyAsText()
+    val jsonElement = Json.parseToJsonElement(json)
+    val joke = jsonElement.jsonObject["value"]
+    println(jsonElement)
+    println(joke)
     client.close()
 }
