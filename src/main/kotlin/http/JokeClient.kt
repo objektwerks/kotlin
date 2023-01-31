b5b4790f304396eeb9cd6f0ea7ff72830d11dc5c
objektwerks <objektwerks@runbox.com>
2023-01-31 08:37:20 -0500

    edited joke client url

--- src/main/kotlin/http/JokeClient.kt
+++ src/main/kotlin/http/JokeClient.kt
@@ -7,7 +7,7 @@ import io.ktor.client.statement.*
 
 suspend fun main() {
     val client = HttpClient(CIO)
-    val response: HttpResponse = client.get("http://api.icndb.com/jokes/random/")
+    val response: HttpResponse = client.get("https://api.chucknorris.io/jokes/random")
     println(response.status)
     println(response.bodyAsText())
     client.close()
