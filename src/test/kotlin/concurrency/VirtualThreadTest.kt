9a718d52399227c1587800451f9b5c0c0da37cf4
objektwerks <objektwerks@runbox.com>
2023-02-01 08:08:30 -0500

    added virtual thread test

---
+++ src/test/kotlin/concurrency/VirtualThreadTest.kt
@@ -0,0 +1,29 @@
+package concurrency
+
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Future
+
+import org.junit.Test
+import java.util.concurrent.Executors
+
+/**
+ * Virtual Threads: openjdk.org/jeps/425
+ * Article: www.marcobehler.com/guides/java-project-loom
+ */
+class VirtualThreadTest {
+    @Test
+    @Throws(ExecutionException::class, InterruptedException::class)
+    fun virtualThreadTest() {
+        val tasks = ArrayList<FileLineCountTask>(2)
+        tasks.add(FileLineCountTask("./data/data.a.csv"))
+        tasks.add(FileLineCountTask("./data/data.b.csv"))
+        var lines = 0
+        Executors.newVirtualThreadPerTaskExecutor().use { executor ->
+            val futures: List<Future<Int>> = executor.invokeAll(tasks)
+            for (future in futures) {
+                lines += future.get()
+            }
+        }
+        assert(lines == 540959)
+    }
+}
