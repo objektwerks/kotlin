27651a70643a0e3349d520e003c420d663640516
objektwerks <objektwerks@runbox.com>
2023-02-03 01:38:36 -0500

    centered chuck norris fx app

--- src/main/kotlin/javafx/ChuckNorrixFxApp.kt
+++ src/main/kotlin/javafx/ChuckNorrixFxApp.kt
@@ -1,11 +1,19 @@
 package javafx
 
 import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Label
 import javafx.stage.Stage
 
 class ChuckNorrixFxApp : Application() {
     override fun start(primaryStage: Stage) {
         primaryStage.title = "Chuck Norris Jokes"
+
+        val label = Label("Test joke!")
+        val scene = Scene(label, 400.0, 300.0)
+
+        primaryStage.scene = scene
+        primaryStage.centerOnScreen()
         primaryStage.show()
     }
 }
