dbbbc06355f0885d3e567d3805e23e80f6da1656
objektwerks <objektwerks@runbox.com>
2023-02-03 01:42:51 -0500

    added chuck norris fx task

--- src/main/kotlin/javafx/ChuckNorrixFxApp.kt
+++ src/main/kotlin/javafx/ChuckNorrixFxApp.kt
@@ -1,9 +1,17 @@
 package javafx
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.control.Label
 import javafx.stage.Stage
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
 
 class ChuckNorrixFxApp : Application() {
     override fun start(primaryStage: Stage) {
@@ -15,6 +23,18 @@ class ChuckNorrixFxApp : Application() {
         primaryStage.scene = scene
         primaryStage.centerOnScreen()
         primaryStage.show()
+    }
+}
+
+class ChuckNorrisFxTask {
+    private val client = HttpClient(CIO)
+
+    private suspend fun getJoke(): String {
+        return runCatching {
+            val json = client.get("https://api.chucknorris.io/jokes/random").bodyAsText()
+            val jsonElement = Json.parseToJsonElement(json)
+            jsonElement.jsonObject["value"].toString()
+        }.getOrDefault("Chuck is taking a power nap. Come back later.")
     }
 }
 
