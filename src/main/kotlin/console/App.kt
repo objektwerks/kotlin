d309c09914f98dbff06a3ab07083ac55261ecd22
objektwerks <objektwerks@runbox.com>
2023-02-04 19:32:30 -0500

    refactored app

--- src/main/kotlin/console/App.kt
+++ src/main/kotlin/console/App.kt
@@ -2,12 +2,14 @@ package console
 
 import java.time.Instant
 
-object App {
-    private fun greeting(): String = "Kotlin!"
+class App {
+    companion object {
+        private fun greeting(): String = "Kotlin!"
 
-    @JvmStatic fun main(args : Array<String>) {
-        args.forEach { arg -> println("Arg: $arg") }
-        println("Greetings, ${greeting()}")
-        println("Datetime: ${Instant.now()}")
+        @JvmStatic fun main(args : Array<String>) {
+            args.forEach { arg -> println("Arg: $arg") }
+            println("Greetings, ${greeting()}")
+            println("Datetime: ${Instant.now()}")
+        }
     }
 }
