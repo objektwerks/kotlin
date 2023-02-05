e24f3e4089076dcc7a430f526c87cab3d1bb9210
objektwerks <objektwerks@runbox.com>
2023-02-05 09:43:21 -0500

    edited result test

--- src/test/kotlin/stdlib/ResultTest.kt
+++ src/test/kotlin/stdlib/ResultTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 
 class ResultTest {
     @Test fun result() {
-        val validDateTime = "2021-04-04T12:21:41.00Z"
+        val validDateTime = "2021-02-04T12:21:41.00Z"
         val validInstant = Instant.parse(validDateTime)
 
         assert(
