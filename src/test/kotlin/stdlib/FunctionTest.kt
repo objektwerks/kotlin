49206746d886a0a9a891f6237b726022841583ea
objektwerks <objektwerks@runbox.com>
2023-02-07 14:14:46 -0500

    reformatted and refactored function test

--- src/test/kotlin/stdlib/FunctionTest.kt
+++ src/test/kotlin/stdlib/FunctionTest.kt
@@ -8,22 +8,25 @@ data class Values(val items: List<String>)
 val Values.delimitted: String get() = items.joinToString()
 
 class FunctionTest {
-    // Lambdas. Anonymous or literal functions.
+    // Lambdas
     val sum = { x: Int, y: Int -> x + y }
     val multiply: (Int, Int) -> Int = { x: Int, y: Int -> x * y }
 
     @Test fun function() {
         fun square(n: Int): Int = n * n
+
         assert(square(2) == 4)
     }
 
     @Test fun genericFunction() {
         fun <T> asSingletonList(item: T): List<T> = listOf(item)
+
         assert(asSingletonList(3) == listOf(3))
     }
 
     @Test fun functionExtension() {
         fun String.addBang(): String = "$this!"
+
         assert("hello".addBang() == "hello!")
     }
 
@@ -34,6 +37,7 @@ class FunctionTest {
 
     @Test fun higherOrder() {
         fun total(x: Int, y: Int, f: (Int, Int) -> Int): Int = f(x, y)
+
         assert(total(3, 3, sum) == 6)
         assert(total(3, 3, multiply) == 9)
     }
@@ -48,6 +52,7 @@ class FunctionTest {
             infix fun square(n: Int): Int = n * n
             infix fun cube(n: Int): Int = n * n * n
         }
+
         val numbers = Numbers()
         assert(numbers square 3 == 9)
         assert(numbers cube 3 == 27)
@@ -55,6 +60,7 @@ class FunctionTest {
 
     @Test fun operator() {
         operator fun Int.times(value: String) = value.repeat(this)
+
         assert(2 * "m" == "mm")
     }
 
@@ -78,16 +84,14 @@ class FunctionTest {
 
     @Test fun defaultValue() {
         fun divide(x: Int, y: Int = 1): Int = x / y
+
         assert(divide(4) == 4)
         assert(divide(4, 2) == 2)
     }
 
     @Test fun varargs() {
-        fun <T> toList(vararg ts: T): List<T> {
-            val result = ArrayList<T>()
-            for (t in ts) result.add(t)
-            return result
-        }
+        fun <T> toList(vararg ts: T): List<T> = mutableListOf<T>( *ts )
+
         assert(toList(1, 2, 3) == listOf(1, 2, 3))
     }
 
@@ -95,6 +99,7 @@ class FunctionTest {
         tailrec fun factorial(n: Int, acc: Int = 1): Int =
             if (n == 1) acc
             else factorial(n - 1, acc * n)
+
         assert(factorial(9) == 362880)
     }
 
@@ -126,6 +131,7 @@ class FunctionTest {
         class Service(val host: String, val port: Int) {
             fun query(): String = "hello!"
         }
+
         val service = Service("github.com", 80)
         service.run {
             assert( host == "github.com" )
@@ -136,6 +142,7 @@ class FunctionTest {
 
     @Test fun applyScope() { // this scope
         class Dog(var kind: String, var age: Int = 1, var name: String = "")
+
         val dog = Dog(kind = "Bulldog").apply {
             age = 1
             name = "Trouble"
