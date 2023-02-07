a7927c467e0f9e43c24c34bd2afef882ac94fc6d
objektwerks <objektwerks@runbox.com>
2023-02-07 07:13:12 -0500

    extracted match test from function test, put into when test

--- src/test/kotlin/stdlib/ControlFlowTest.kt
+++ src/test/kotlin/stdlib/ControlFlowTest.kt
@@ -39,30 +39,6 @@ class ControlFlowTest {
         assert( b == 3 )
     }
 
-    @Test fun match() {
-        when (val n = Random(10).nextInt().absoluteValue) {
-            0 -> fail("number equals zero!")
-            else -> assert( n > 10 )
-        }
-
-        val result = when (val n = Random(10).nextInt().absoluteValue) {
-            in 0..10 -> fail("number is between 0 and 10!")
-            else -> n
-        }
-        assert( result > 10 )
-
-        fun isTypeOf(typeOf: Any): String = when(typeOf) {
-            is String -> "s"
-            is Int -> "i"
-            is Boolean -> "2"
-            else -> ""
-        }
-        assert(isTypeOf("string") == "s")
-        assert(isTypeOf(1) == "i")
-        assert(isTypeOf(true) == "2")
-        assert(isTypeOf(1.0) == "")
-    }
-
     @Test fun repeat() {
         var sum = 0
         repeat(3) {
