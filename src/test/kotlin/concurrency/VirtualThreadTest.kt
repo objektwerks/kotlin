e6db4f406b4dcccf971447b834b55e86014314a0
objektwerks <objektwerks@runbox.com>
2023-02-07 14:35:41 -0500

    refactored virtual thread test

--- src/test/kotlin/concurrency/VirtualThreadTest.kt
+++ src/test/kotlin/concurrency/VirtualThreadTest.kt
@@ -2,7 +2,6 @@ package concurrency
 
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.Executors
-import java.util.concurrent.Future
 
 import org.junit.Test
 
@@ -18,12 +17,8 @@ class VirtualThreadTest {
         val tasks = ArrayList<FileLineCountTask>(2)
         tasks.add(FileLineCountTask("./data/data.a.csv"))
         tasks.add(FileLineCountTask("./data/data.b.csv"))
-        var lines = 0
-        Executors.newVirtualThreadPerTaskExecutor().use { executor ->
-            val futures: List<Future<Int>> = executor.invokeAll(tasks)
-            for (future in futures) {
-                lines += future.get()
-            }
+        val lines = Executors.newVirtualThreadPerTaskExecutor().use { executor ->
+            executor.invokeAll(tasks).sumOf { future -> future.get() }
         }
         assert(lines == 540959)
     }
