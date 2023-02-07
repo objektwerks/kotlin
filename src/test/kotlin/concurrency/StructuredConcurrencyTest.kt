defc88d112454011bbd34049393007997d80377f
objektwerks <objektwerks@runbox.com>
2023-02-07 14:31:40 -0500

    refactored structured concurrency test

--- src/test/kotlin/concurrency/StructuredConcurrencyTest.kt
+++ src/test/kotlin/concurrency/StructuredConcurrencyTest.kt
@@ -1,8 +1,9 @@
 package concurrency
 
-import jdk.incubator.concurrent.StructuredTaskScope.ShutdownOnFailure
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.Future
+
+import jdk.incubator.concurrent.StructuredTaskScope.ShutdownOnFailure
 
 import org.junit.Test
 
@@ -14,13 +15,12 @@ class StructuredConcurrencyTest {
     @Test
     @Throws(ExecutionException::class, InterruptedException::class)
     fun structuredConcurrencyTest() {
-        var lines: Int
-        ShutdownOnFailure().use { scope ->
+        val lines = ShutdownOnFailure().use { scope ->
             val alines: Future<Int> = scope.fork { FileLineCountTask("./data/data.a.csv").call() }
             val blines: Future<Int> = scope.fork { FileLineCountTask("./data/data.b.csv").call() }
             scope.join()
             scope.throwIfFailed()
-            lines = alines.resultNow() + blines.resultNow()
+            alines.resultNow() + blines.resultNow()
         }
         assert(lines == 540959)
     }
