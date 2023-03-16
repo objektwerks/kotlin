c0cb37b7f395d1ea82e9b3a42a7032e8428c5c38
objektwerks <objektwerks@runbox.com>
2023-03-16 13:35:00 -0400

    upgraded kotlinx-serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
     implementation("io.ktor:ktor-client-core:2.2.1")
     implementation("io.ktor:ktor-client-cio:2.2.1")
