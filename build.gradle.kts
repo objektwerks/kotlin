deb00fe8110cb3933e6dfcbea34e1447a7023f5e
objektwerks <objektwerks@runbox.com>
2023-04-13 10:01:55 -0400

    edited build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,10 +47,10 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
-    implementation("io.ktor:ktor-client-core:2.2.4")
-    implementation("io.ktor:ktor-client-cio:2.2.4")
-    implementation("io.ktor:ktor-server-core:2.2.4")
-    implementation("io.ktor:ktor-server-netty:2.2.4")
+    implementation("io.ktor:ktor-client-core:2.3.0-eap-624")
+    implementation("io.ktor:ktor-client-cio:2.3.0-eap-624")
+    implementation("io.ktor:ktor-server-core:2.3.0-eap-625")
+    implementation("io.ktor:ktor-server-netty:2.3.0-eap-624")
 
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
