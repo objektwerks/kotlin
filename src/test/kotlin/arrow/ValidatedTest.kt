edafe2494eaf6525bf803efdf47b429975abd9a8
objektwerks <objektwerks@runbox.com>
2023-11-01 13:02:34 -0400

    refactored validated test

--- src/test/kotlin/arrow/ValidatedTest.kt
+++ src/test/kotlin/arrow/ValidatedTest.kt
@@ -13,29 +13,29 @@ sealed class PersonError {
 }
 
 object PersonValidator {
-    private fun String.validatedName(): Validated<Nel<PersonError>, String> =
-        if (this.isNotEmpty()) this.valid()
-        else PersonError.InvalidName(this).nel().invalid()
+    private fun String.validatedName(): Either<NonEmptyList<PersonError.InvalidName>, String> =
+        if (this.isNotEmpty()) this.right()
+        else PersonError.InvalidName(this).nel().left()
 
-    private fun Int.validatedAge(): Validated<Nel<PersonError>, Int> =
-        if (this > 0) this.valid()
-        else PersonError.InvalidAge(this).nel().invalid()
+    private fun Int.validatedAge(): Either<NonEmptyList<PersonError.InvalidAge>, Int> =
+        if (this > 0) this.right()
+        else PersonError.InvalidAge(this).nel().left()
 
-    fun validate(person: Person): Validated<Nel<PersonError>, Person> =
-        if (person.name.validatedName().isValid && person.age.validatedAge().isValid) person.valid()
-        else PersonError.InvalidPerson(person).nel().invalid()
+    fun validate(person: Person): Either<NonEmptyList<PersonError>, Person> =
+        if (person.name.validatedName().isRight() && person.age.validatedAge().isRight()) person.right()
+        else PersonError.InvalidPerson(person).nel().left()
 }
 
 class ValidatedTest {
     @Test fun valid() {
         val person = Person("Fred Flintstone", 37)
         val result = PersonValidator.validate(person)
-        assert(result.isValid)
+        assert(result.isRight())
     }
 
     @Test fun invalid() {
         val person = Person("", 0)
         val result = PersonValidator.validate(person)
-        assert(result.isInvalid)
+        assert(result.isLeft())
     }
 }
