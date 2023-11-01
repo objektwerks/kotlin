93a7269fbeb75fd1581660635ba112684f1dfd91
objektwerks <objektwerks@runbox.com>
2023-11-01 06:28:38 -0400

    upgraded test logger plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.serialization") version "1.9.20"
     application
-    id("com.adarshr.test-logger") version "3.2.0"
+    id("com.adarshr.test-logger") version "4.0.0"
 }
 
 java {
