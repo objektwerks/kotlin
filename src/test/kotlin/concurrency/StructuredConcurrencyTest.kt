598805f80e9dff258fea62f55d56b9b8f7d34d64
objektwerks <objektwerks@runbox.com>
2023-11-01 12:50:09 -0400

    refactored structured concurrency test

--- src/test/kotlin/concurrency/StructuredConcurrencyTest.kt
+++ src/test/kotlin/concurrency/StructuredConcurrencyTest.kt
@@ -2,25 +2,20 @@ package concurrency
 
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.Future
-
-import jdk.incubator.concurrent.StructuredTaskScope.ShutdownOnFailure
+import java.util.concurrent.StructuredTaskScope
 
 import org.junit.Test
 
-/**
- * Configure in gradle: --enable-preview --add-modules jdk.incubator.concurrent
- * Structured Concurrency: openjdk.org/jeps/428
- */
 class StructuredConcurrencyTest {
     @Test
     @Throws(ExecutionException::class, InterruptedException::class)
     fun structuredConcurrencyTest() {
-        val lines = ShutdownOnFailure().use { scope ->
-            val alines: Future<Int> = scope.fork { FileLineCountTask("./data/data.a.csv").call() }
-            val blines: Future<Int> = scope.fork { FileLineCountTask("./data/data.b.csv").call() }
+        val lines = StructuredTaskScope.ShutdownOnFailure().use { scope ->
+            val alines = scope.fork { FileLineCountTask("./data/data.a.csv").call() }
+            val blines = scope.fork { FileLineCountTask("./data/data.b.csv").call() }
             scope.join()
             scope.throwIfFailed()
-            alines.resultNow() + blines.resultNow()
+            alines.get() + blines.get()
         }
         assert(lines == 540959)
     }
