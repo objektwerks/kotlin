06fee17f85c7d2c11f09187588648170025bc077
objektwerks <objektwerks@runbox.com>
2024-01-25 06:44:44 -0500

    enhanced immmutable collection test

--- src/test/kotlin/immutable/ImmutableCollectionTest.kt
+++ src/test/kotlin/immutable/ImmutableCollectionTest.kt
@@ -31,5 +31,6 @@ class ImmutableCollectionTest {
 
     @Test fun conversion() {
         assert( listOf(1, 2, 3).toImmutableList() == persistentListOf(1, 2, 3) )
+        assert( setOf(1, 2, 3).toImmutableSet() == persistentSetOf(1, 2, 3) )
     }
 }
