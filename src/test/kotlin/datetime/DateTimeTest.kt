a72dd03347ef801580fd2e7bbe7efb91564a419a
objektwerks <objektwerks@runbox.com>
2024-01-25 12:04:41 -0500

    enhanced date time test

--- src/test/kotlin/datetime/DateTimeTest.kt
+++ src/test/kotlin/datetime/DateTimeTest.kt
@@ -25,10 +25,10 @@ class DateTimeTest {
         val knownTime = LocalTime(hour = 23, minute = 59, second = 59)
         assert( knownTime.toOption().isSome() )
 
-        assert( "2010-06-01T22:19:44.475Z".toInstant().toOption().isSome() )
-        assert( "2010-06-01T22:19:44".toLocalDateTime().toOption().isSome() )
-        assert( "2010-06-01".toLocalDate().toOption().isSome() )
+        assert( "2020-12-01T22:19:44.475Z".toInstant().toOption().isSome() )
+        assert( "2020-12-01T22:19:44".toLocalDateTime().toOption().isSome() )
+        assert( "2020-12-01".toLocalDate().toOption().isSome() )
         assert( "12:01:03".toLocalTime().toOption().isSome() )
-        assert( "12:0:03.999".toLocalTime().toOption().isSome() )
+        assert( "12:01:03.999".toLocalTime().toOption().isSome() )
     }
 }
