e681524447690d1d0aa5806d1f76af04ea21f3ff
objektwerks <objektwerks@runbox.com>
2024-01-25 11:44:57 -0500

    enhanced date time test

--- src/test/kotlin/datetime/DateTimeTest.kt
+++ src/test/kotlin/datetime/DateTimeTest.kt
@@ -9,9 +9,13 @@ class DateTimeTest {
     @Test fun datetime() {
         val now = Clock.System.now()
         val local = now.toLocalDateTime(TimeZone.currentSystemDefault())
+
         val date = local.date
         val time = local.time
         assert( date.toOption().isSome() )
         assert( time.toOption().isSome() )
+
+        val knownDate = LocalDate(year = 2020, monthNumber = 1, dayOfMonth = 1)
+        assert( knownDate.toOption().isSome() )
     }
 }
