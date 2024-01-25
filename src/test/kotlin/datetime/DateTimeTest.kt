79ea2c93e1a843118f52e6d1f545b11c2ec89a27
objektwerks <objektwerks@runbox.com>
2024-01-25 10:23:40 -0500

    enhanced date time test

--- src/test/kotlin/datetime/DateTimeTest.kt
+++ src/test/kotlin/datetime/DateTimeTest.kt
@@ -1,8 +1,20 @@
 package datetime
 
+import arrow.core.toOption
+import kotlinx.datetime.*
+
 import org.junit.Test
 
 class DateTimeTest {
+    @Test fun datetime() {
+        val now = Clock.System.now()
+        val local = now.toLocalDateTime(TimeZone.currentSystemDefault())
+        val date = local.date
+        val time = local.time
+        assert( date.toOption().isNone() )
+        assert( time.toOption().isNone() )
+    }
+
     @Test fun date() {
     }
 
