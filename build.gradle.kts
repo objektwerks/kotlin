2e1f7799288f2e0df22ff126ed10df40a15c1d4c
objektwerks <objektwerks@runbox.com>
2024-01-25 05:39:02 -0500

    added kotlinx-collections-immutable to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     implementation("io.ktor:ktor-client-core:2.3.7")
