c3718789b36d15687fd27e58efd4c02bfd6bf197
objektwerks <objektwerks@runbox.com>
2024-01-25 09:54:48 -0500

    edited build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.0-Beta3")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")s
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     implementation("io.ktor:ktor-client-core:2.3.7")
