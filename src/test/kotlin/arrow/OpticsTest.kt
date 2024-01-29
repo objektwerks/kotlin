c7d7c80f8ac3f834d79ec7d7d184ae00100a5da7
objektwerks <objektwerks@runbox.com>
2024-01-29 12:44:49 -0500

    edited optics test

--- src/test/kotlin/arrow/OpticsTest.kt
+++ src/test/kotlin/arrow/OpticsTest.kt
@@ -4,24 +4,26 @@ import arrow.optics.optics
 
 import kotlin.test.Test
 
+@optics data class Customer(val name: String, val age: Int, val address: Address) {
+    companion object
+}
+@optics data class Address(val street: Street, val city: City) {
+    companion object
+}
+@optics data class Street( val number: Int, val name: String) {
+    companion object
+}
+@optics data class City(val name: String, val country: String) {
+    companion object
+}
+
 class OpticsTest {
-    @optics data class Person(val name: String, val age: Int, val address: Address) {
-        companion object
-    }
-    @optics data class Address(val street: Street, val city: City) {
-        companion object
-    }
-    @optics data class Street(val name: String, val number: Int?) {
-        companion object
-    }
-    @optics data class City(val name: String, val country: String) {
-        companion object
-    }
-
-    fun Person.capitalizeCountryModify(): Person =
-        Person.address.city.country.modify(this) { it.capitalize() }
-
-    @Test fun modify() {
-
+    @Test fun copy() {
+        val city = City(name = "tampa", country = "US")
+        val street = Street(number = 1, name = "stone")
+        val address = Address(street = street, city = city)
+        val customer = Customer(name = "fred", age = 24, address)
+        val modifiedCustomer = customer.copy(name = "Fred")
+        assert( modifiedCustomer.name.first() == 'F')
     }
 }
