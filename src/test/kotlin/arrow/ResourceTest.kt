e4657a764bc44fa367daea77216ee01fccea91fe
objektwerks <objektwerks@runbox.com>
2024-01-29 10:53:52 -0500

    enhanced resource test

--- src/test/kotlin/arrow/ResourceTest.kt
+++ src/test/kotlin/arrow/ResourceTest.kt
@@ -1,9 +1,31 @@
 package arrow
 
+import arrow.fx.coroutines.resource
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
 import kotlin.test.Test
 
 class ResourceTest {
-    @Test fun resource() {
+    class DataSource {
+        fun connect(): Unit = println("Connecting to datasource...")
+        fun close(): Unit = println("Closed datasource connection.")
+        fun query(): List<String> = listOf("a", "b", "c")
+    }
 
+    @Test
+    fun resource() {
+        val dataSourceAsResource = resource( {
+            DataSource().also { it.connect() }
+        } ) { ds, exitCase ->
+            println("Releasing $ds with exit: $exitCase")
+            withContext(Dispatchers.IO) { ds.close() }
+        }
+        val result = runBlocking {
+            dataSourceAsResource.use { ds -> ds.query() }
+        }
+        assert( result.size == 3 )
     }
 }
