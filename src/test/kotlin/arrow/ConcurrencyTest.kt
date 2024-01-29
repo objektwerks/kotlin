1a7b82b8c0538eb07f9af7d64e0661047aacbde9
objektwerks <objektwerks@runbox.com>
2024-01-29 08:34:52 -0500

    enhanced concurrency test

--- src/test/kotlin/arrow/ConcurrencyTest.kt
+++ src/test/kotlin/arrow/ConcurrencyTest.kt
@@ -1,9 +1,23 @@
 package arrow
 
+import arrow.fx.coroutines.parZip
+
 import kotlin.test.Test
 
-class ConcurrencyTest {
-    @Test fun concurrency() {
+import kotlinx.coroutines.runBlocking
 
+class ConcurrencyTest {
+    private suspend fun parSum(x: Int, y: Int): Int =
+        parZip(
+            { x },
+            { y }
+        ) { a, b -> a + b }
+
+    @Test fun concurrency() {
+        assert(
+            runBlocking {
+                parSum(1, 2) == 3
+            }
+        )
     }
 }
