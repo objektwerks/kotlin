205001074ec0ea5b0f0cea17da8bc1dd5f1eea98
objektwerks <objektwerks@runbox.com>
2024-01-29 12:11:08 -0500

    enhanced optics test

--- src/test/kotlin/arrow/OpticsTest.kt
+++ src/test/kotlin/arrow/OpticsTest.kt
@@ -1,8 +1,23 @@
 package arrow
 
+import arrow.optics.optics
+
 import kotlin.test.Test
 
 class OpticsTest {
+    @optics data class Person(val name: String, val age: Int, val address: Address) {
+        companion object
+    }
+    @optics data class Address(val street: Street, val city: City) {
+        companion object
+    }
+    @optics data class Street(val name: String, val number: Int?) {
+        companion object
+    }
+    @optics data class City(val name: String, val country: String) {
+        companion object
+    }
+
     @Test fun optics() {
 
     }
