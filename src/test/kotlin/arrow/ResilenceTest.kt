737f0ffe8d59b8d7e3094198051f165117420c62
objektwerks <objektwerks@runbox.com>
2024-01-30 08:09:42 -0500

    edited resilence test

--- src/test/kotlin/arrow/ResilenceTest.kt
+++ src/test/kotlin/arrow/ResilenceTest.kt
@@ -1,9 +1,15 @@
 package arrow
 
+import arrow.fx.coroutines.Schedule
+
 import kotlin.test.Test
 
 class ResilenceTest {
-    @Test fun copy() {
-
+    @Test fun recur() {
+        var counter = 0
+        Schedule.recurs<Unit>(3).repeat { // 1 + 3
+            counter++
+        }
+        assert( counter == 4)
     }
 }
