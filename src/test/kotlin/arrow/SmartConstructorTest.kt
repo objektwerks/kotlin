12aac405d018f3be693ee7288fba211c55de21b8
objektwerks <objektwerks@runbox.com>
2024-02-01 16:13:09 -0500

    implemented smart constructor test

--- src/test/kotlin/arrow/SmartConstructorTest.kt
+++ src/test/kotlin/arrow/SmartConstructorTest.kt
@@ -1,7 +1,24 @@
 package arrow
 
+import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+
 import org.junit.Test
 
-class SmartConstructorTest {
+object EmptyAuthorName
+data class Author internal constructor(val name: String) {
+    companion object {
+        operator fun invoke(name: String): Either<EmptyAuthorName, Author> = either {
+            ensure(name.isNotEmpty()) { EmptyAuthorName }
+            Author(name)
+        }
+    }
+}
 
+class SmartConstructorTest {
+    @Test fun smartConstructor() {
+        assert( Author.invoke("").isLeft() )
+        assert( Author.invoke("Fred Flintstone").isRight() )
+    }
 }
