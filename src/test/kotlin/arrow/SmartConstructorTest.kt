25701923b0f44e23dd22d059c603fff913d955fb
objektwerks <objektwerks@runbox.com>
2024-02-02 06:10:09 -0500

    corrected zipOrAccumulate error in smart constructor test

--- src/test/kotlin/arrow/SmartConstructorTest.kt
+++ src/test/kotlin/arrow/SmartConstructorTest.kt
@@ -1,8 +1,10 @@
 package arrow
 
 import arrow.core.Either
+import arrow.core.NonEmptyList
 import arrow.core.raise.either
 import arrow.core.raise.ensure
+import arrow.core.raise.zipOrAccumulate
 
 import org.junit.Test
 
@@ -16,10 +18,12 @@ sealed class AuthorError {
  */
 data class Author internal constructor(val name: String, val age: Int) {
     companion object {
-        operator fun invoke(name: String, age: Int): Either<AuthorError, Author> = either {
-            ensure(name.isNotEmpty()) { AuthorError.InvalidName }
-            ensure(age > 0 ) { AuthorError.InvalidAge }
-            Author(name, age)
+        operator fun invoke(name: String, age: Int): Either<NonEmptyList<AuthorError>, Author> = either {
+            zipOrAccumulate(
+                { ensure(name.isNotEmpty()) { AuthorError.InvalidName } },
+                { ensure(age > 0 ) { AuthorError.InvalidAge } }
+            ) { _, _ -> Author(name, age) }
+
         }
     }
 }
