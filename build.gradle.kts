ea1804bd7155d4f2665e1520857039583661b1d5
objektwerks <objektwerks@runbox.com>
2024-03-03 09:02:36 -0500

    upgraded logback classic

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,5 +79,5 @@ dependencies {
 
     implementation("com.h2database:h2:2.2.224")
 
-    implementation("ch.qos.logback:logback-classic:1.5.0")
+    implementation("ch.qos.logback:logback-classic:1.5.1")
 }
