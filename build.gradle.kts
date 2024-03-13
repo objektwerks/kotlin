4178d84407b72c76fc13de4c2f00d8259a558dc7
objektwerks <objektwerks@runbox.com>
2024-03-13 12:39:47 -0400

    upgraded ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     kotlin("plugin.serialization") version "2.0.0-Beta4"
     application
     id("com.adarshr.test-logger") version "4.0.0"
-    id("com.google.devtools.ksp") version "2.0.0-Beta4-1.0.17"
+    id("com.google.devtools.ksp") version "2.0.0-Beta4-1.0.19"
 }
 
 java {
