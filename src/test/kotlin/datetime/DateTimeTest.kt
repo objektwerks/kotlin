a72a80c2f3fd43ab6d8458b2c2d784c1e0b7d521
objektwerks <objektwerks@runbox.com>
2024-05-22 15:15:47 -0400

    correcting datetime test

--- src/test/kotlin/datetime/DateTimeTest.kt
+++ src/test/kotlin/datetime/DateTimeTest.kt
@@ -28,7 +28,7 @@ class DateTimeTest {
 
         assert( Instant.parse("2020-12-01T22:20:40.475Z").toOption().isSome() )
         assert( LocalDateTime.parse("2020-12-01T22:20:40").toOption().isSome() )
-        assert( "2020-12-01".toLocalDate().toOption().isSome() )
+        assert( LocalDate.parse("2020-12-01").toOption().isSome() )
         assert( "12:01:03".toLocalTime().toOption().isSome() )
         assert( "12:01:03.999".toLocalTime().toOption().isSome() )
 
