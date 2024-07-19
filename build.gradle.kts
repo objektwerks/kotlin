af9e6cf0beca6730b77b533bc2f6bac4ed1f19fc
objektwerks <objektwerks@runbox.com>
2024-07-19 09:16:20 -0400

    upgraded kotlinx coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
