698b243563978191bf04ceb0510139ee7e070c8f
Objektwerks <objektwerks@runbox.com>
2024-11-26 11:11:34 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     kotlin("plugin.serialization") version "2.1.0-RC2"
     application
     id("com.adarshr.test-logger") version "4.0.0"
-    id("com.google.devtools.ksp") version "2.1.0-RC-1.0.27"
+    id("com.google.devtools.ksp") version "2.1.0-RC2-1.0.28"
 }
 
 java {
