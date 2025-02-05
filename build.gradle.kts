10b64d9d14030ca2c735087c6eb2a4621f73767e
Objektwerks <objektwerks@runbox.com>
2025-02-05 07:24:51 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 dependencies {
     val kotlinVersion = "2.1.10"
     val ktorVersion = "3.0.3"
-    val exposedVersion = "0.58.0"
+    val exposedVersion = "0.59.0"
     val arrowVersion = "2.0.1"
     val hopliteVersion = "2.9.0"
 
