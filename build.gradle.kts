f193554086c4921a2e06114f7a272084aafa5495
Objektwerks <objektwerks@runbox.com>
2025-06-24 11:06:50 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 
     implementation("io.ktor:ktor-client-core:$ktorVersion")
