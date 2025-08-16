f7ddfcd99fc7cd4d7f4cad9cdf48fd218ee29153
Objektwerks <objektwerks@runbox.com>
2025-08-16 08:09:32 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ group = "kotlin"
 version = "1.3-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "2.2.0"
-    kotlin("plugin.serialization") version "2.2.0"
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.serialization") version "2.2.10"
     application
     id("com.adarshr.test-logger") version "4.0.0"
     id("com.google.devtools.ksp") version "2.2.0-2.0.2"
@@ -37,7 +37,7 @@ repositories {
 }
 
 dependencies {
-    val kotlinVersion = "2.2.0"
+    val kotlinVersion = "2.2.10"
     val ktorVersion = "3.2.3"
     val exposedVersion = "1.0.0-beta-5"
     val arrowVersion = "2.1.2"
