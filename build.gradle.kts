6c0b7269e8d30b94697b690b181b44f22f40b522
Objektwerks <objektwerks@runbox.com>
2026-05-18 08:44:19 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
 
 dependencies {
     val kotlinVersion = "2.4.0-Beta2"
-    val ktorVersion = "3.4.3"
+    val ktorVersion = "3.5.0"
     val exposedVersion = "0.61.0" // Don't upgrade!
     val arrowVersion = "2.2.2.1"
     val hopliteVersion = "3.0.0.RC3"
